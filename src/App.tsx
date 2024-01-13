d135691a621d2bd8cc77f498e44332a92ab32df6
Phantom <phanton7487@gmail.com>
2024-01-13 22:27:37 +0200

    lesons 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from './Todolist';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+
+    const task1 = [
+        {id:0, title:"HTML-CSS", isDone:false},
+        {id:1, title:"React", isDone:true},
+        {id:2, title:"Js" ,isDone:false},
+        {id:3, title:"Redux" ,isDone:false},
+    ]
+
+    const task2 = [
+        {id:0, title:"MILK", isDone:false},
+        {id:1, title:"BREAD", isDone:true},
+        {id:2, title:"Eat" ,isDone:false},
+    ]
+    const task3: { id: number; title: string; isDone: boolean; }[] = [
+
+    ]
+    return (
+        <div className='App'>
+
+        <Todolist title='What to Learn' tasks={task1}/>
+        <Todolist title='What to buy' tasks={task2}/>
+        <Todolist title='What to buy' tasks={task3}/>
+
+        </div>)
 }
 
 export default App;
