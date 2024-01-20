8559c067825081b1cef8265784401625109f6b41
Phantom <phanton7487@gmail.com>
2024-01-20 20:41:33 +0200

    lesons 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,45 @@
-import React from 'react';
-import './App.css';
-import {Todolist} from './Todolist';
-
+import React, {useState} from 'react';
+import s from './App.module.css';
+import {TaskType, Todolist} from './Todolist';
+type FilterType= 'all'|'active'|'completed'
 function App () {
-
-    const task1 = [
+const [task,setTask1] = useState<Array<TaskType>>(
+    [
         {id:0, title:"HTML-CSS", isDone:false},
         {id:1, title:"React", isDone:true},
         {id:2, title:"Js" ,isDone:false},
         {id:3, title:"Redux" ,isDone:false},
     ]
+)
+    const [tasks2, setTasks2] = useState<Array<TaskType>>([
+        { id: 0, title: "MILK", isDone: false },
+        { id: 1, title: "BREAD", isDone: true },
+        { id: 2, title: "Eat", isDone: false },
+    ]);
 
-    const task2 = [
-        {id:0, title:"MILK", isDone:false},
-        {id:1, title:"BREAD", isDone:true},
-        {id:2, title:"Eat" ,isDone:false},
-    ]
-    const task3: { id: number; title: string; isDone: boolean; }[] = [
+const removeTask = (taskId:number) => {
 
-    ]
+}
+    const [tasks3, setTasks3] = useState([]);
+const [filter,setFilter]= useState<FilterType>('all')
+    const FilterTask = (alltask:Array<TaskType>,filterValue:FilterType)=>{
+switch (filterValue){
+    case "active": return alltask.filter(t=>!t.isDone )
+    case "completed": return alltask.filter(t=>t.isDone )
+    default:return  alltask
+}
+    }
+const filtredTask = FilterTask(task,filter)
     return (
-        <div className='App'>
+        <div  className={s.backgroundIMG}>
+            <div className={s.App}>
 
-        <Todolist title='What to Learn' tasks={task1}/>
-        <Todolist title='What to buy' tasks={task2}/>
-        <Todolist title='What to buy' tasks={task3}/>
+                <Todolist title='What to Learn' tasks={filtredTask} removeTask={removeTask}/>
+                {/*<Todolist title='What to buy' tasks={tasks2}  removeTask={removeTask}/>
+                <Todolist title='What to buy' tasks={tasks3}  removeTask={removeTask}/>*/}
+
+            </div>
+
 
         </div>)
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,29 +1,56 @@
 import React, {JSX} from 'react';
+import s from './todolist.module.css';
 
-type TaskType = {
-    id: number
-    title: string
-    isDone: boolean
-}
+export type TaskType = {
+    id: number;
+    title: string;
+    isDone: boolean;
+};
 
 type PropsType = {
-    title: string
-    tasks: Array<TaskType>
-}
+    title: string;
+    tasks: Array<TaskType>;
+
+};
+
+export const Todolist: React.FC<PropsType> =
+    ({tasks,title,removeTask})=> {
+   /* const removeTaskHandler = (taskId:number) => {
+        removeTask(taskId)
+    }*/
+        const NewArray: Array<JSX.Element> = tasks.map((newLis) => {
 
 
-export function Todolist(props: PropsType) {
-  let tast
-    return <div>
-        <div>
-            <input/>
-            <button>X</button>
+            return (
+            <li key={newLis.id}>
+                <button className={s.deleteButton} >X</button>
+                <input type='checkbox' checked={newLis.isDone}></input>
+                <span>{newLis.title}</span>
+            </li>
+        );
+    });
+
+
+    const tasksLists: JSX.Element = tasks.length ?
+        <ul className={s.taskList}>{NewArray}</ul>
+        : <span>You taskList is Empty</span>
+
+
+    return (
+        <div className={s.todoCardWrapper}>
+            <div className={s.inputContainer}>
+             <h3>{title}</h3>
+                <input />
+                <button className={s.deleteButton} >X</button>
+            </div>
+
+
+            {tasksLists}
+            <div className={s.buttonContainer}>
+                <button className={s.todoButton}>ALL</button>
+                <button className={s.todoButton}>ACTIVE</button>
+                <button className={s.todoButton}> Completed</button>
+            </div>
         </div>
-        <ul> {tasksLists}</ul>
-        <div>
-            <button>ALL</button>
-            <button>ACTIVE</button>K
-            <button>Complited</button>
-        </div>
-    </div>
+    );
 }
